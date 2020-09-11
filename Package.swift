dd9efda7bbaeff4f2ed3f7265e4195f553114afe
Pradeep Roark <pradeep.roark@gmail.com>
2020-09-11 22:11:27 +0100

    lower tvOS target from v14 to v13

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     platforms: [
         .iOS(.v14),
         .macOS(.v11),
-        .tvOS(.v14)
+        .tvOS(.v13)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
